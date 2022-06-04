4e756d76311bc9af19db07af72a4cb38776a85cb
Thiago González <thiagogonzalez2000@gmail.com>
2022-06-04 13:23:47 -0300

    Add exercise liqn with lambda to github

---
+++ Exercise_LINQ/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_LINQ.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Exercise_LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise_LINQ.Entities;
+using System.Globalization;
+
+
+List<Product> products = new List<Product>();
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string name = line[0];
+            double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+            products.Add(new Product(name, price));
+        }
+    }
+    var priceAverage = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+    Console.Write("Average price: " + priceAverage.ToString("F2", CultureInfo.InvariantCulture));
+    var priceLowerThenAverage = products.Where(p => p.Price < priceAverage).OrderByDescending(p => p.Name).Select(p => p.Name);
+    Console.WriteLine();
+    Console.WriteLine("Products's name whose price is less than the average price: ");
+    foreach (var product in priceLowerThenAverage)
+    {
+        Console.WriteLine(product);
+    }
+    
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
